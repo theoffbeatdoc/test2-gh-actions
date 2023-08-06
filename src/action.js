0e1ad2e50c6acb669ba482e67a8088adaf9ac73f
The OffBeat Doc | Dr. Koustav Sinha Ray <82215509+theoffbeatdoc@users.noreply.github.com>
2023-08-07 02:03:05 +0530

    Update action.js

--- src/action.js
+++ src/action.js
@@ -4,13 +4,17 @@ const github = require('@actions/github');
 
 async function run() {
   const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-  const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
+// const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
 
-  const randomPos = Math.round(Math.random() * 1000);
-  const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
-  const response = await fetch(url);
-  const { results } = await response.json();
-  const gifUrl = results[0].media[0].tinygif.url;
+  // const randomPos = Math.round(Math.random() * 1000);
+  // const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+  // const response = await fetch(url);
+  // const { results } = await response.json();
+  // const gifUrl = results[0].media[0].tinygif.url;
+
+  const gifUrl = 'https://miro.medium.com/v2/resize:fit:786/format:webp/1*POAUldBjPRY5DCewGpXx8Q.png';
+
+  const octokit = github.getOctokit(GITHUB_TOKEN);
 
   const octokit = github.getOctokit(GITHUB_TOKEN);
 
